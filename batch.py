8546602bc667a6cb06cce3f88e527cd416e05af0
LeoRiether <leonardoalves8217@gmail.com>
2022-03-25 23:25:10 -0300

    trying to debug batch.py

--- batch.py
+++ batch.py
@@ -9,17 +9,19 @@ import os
 def run(debug=False):
 
     params = {
-        "reproduce_prob": np.linspace(0, 0.5, 11),
-        "growback_factor": [0.15],
+        "reproduce_prob": np.linspace(0, 0.65, 12),
+        "growback_factor": 0.15,
     }
 
     iterations = 300
     steps = 100
 
     if debug:
-        params["reproduce_prob"] = [0.15]
+        params["reproduce_prob"] = 0.15
         iterations = 2
         steps = 5
+
+    print("starting results")
 
     results = batch_run(
         SugarscapeCg,
@@ -30,9 +32,15 @@ def run(debug=False):
         data_collection_period=-1,
     )
 
+    print("results ok")
+
     dataframe = pd.DataFrame(results)
 
+    print("dataframe ok")
+
     timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+
+    print("timestamp ok")
 
     # Create csv filename
     fn = "data"
@@ -42,9 +50,14 @@ def run(debug=False):
     #     fn += "_gf{}".format(gf)
     fn += "_t{}.csv".format(timestamp)
 
+    print("fn ok")
+
     os.makedirs("csv", exist_ok=True)
+    print("makedir ok")
     dataframe.drop(['SsAgent'], axis=1).to_csv(os.path.join("csv", 'model_' + fn))
+    print("model_ ok")
     dataframe.drop(['Oscillation', 'Average'], axis=1).to_csv(os.path.join("csv", 'agent_' + fn))
+    print("agent_ ok")
 
 if __name__ == '__main__':
     if sys.argv[-1] == '--debug':

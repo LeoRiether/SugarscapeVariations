3a279ecfa429b2ba5178277fe9f66bf71db21d5f
LeoRiether <leonardoalves8217@gmail.com>
2022-03-26 10:14:20 -0300

    Fix batch splitting

--- batch.py
+++ batch.py
@@ -9,11 +9,11 @@ import os
 def run(split=1, debug=False):
 
     params = {
-        "reproduce_prob": np.linspace(0, 0.65, 12),
+        "reproduce_prob": np.linspace(0, 0.6, 13),
         "growback_factor": 0.15,
     }
 
-    iterations = 300
+    iterations = 500
     steps = 100
 
     if debug:
@@ -21,9 +21,7 @@ def run(split=1, debug=False):
         iterations = 2
         steps = 5
 
-    for i in range((iterations + split - 1) // split):
-        print("starting results")
-
+    for i in range(split):
         results = batch_run(
             SugarscapeCg,
             params,
@@ -33,15 +31,9 @@ def run(split=1, debug=False):
             data_collection_period=-1,
         )
 
-        print("results ok")
-
         dataframe = pd.DataFrame(results)
 
-        print("dataframe ok")
-
         timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-
-        print("timestamp ok")
 
         # Create csv filename
         fn = "data"
@@ -51,20 +43,26 @@ def run(split=1, debug=False):
         #     fn += "_gf{}".format(gf)
         fn += "_t{}.csv".format(timestamp)
 
-        print("fn ok")
-
         os.makedirs("csv", exist_ok=True)
-        print("makedir ok")
         dataframe.drop(['SsAgent'], axis=1).to_csv(os.path.join("csv", 'model_' + fn))
-        print("model_ ok")
         dataframe.drop(['Oscillation', 'Average'], axis=1).to_csv(os.path.join("csv", 'agent_' + fn))
-        print("agent_ ok")
 
+files = [ e.path for e in os.scandir('model') ]
+def join(files):
+    """ Concatena vários dataframes em um só """
+    frame = pd.read_csv(files[0]).drop('Unnamed: 0', axis=1)
+    max_id = frame['RunId'].max()
+    for f in files[1:]:
+        frame2 = pd.read_csv(f).drop('Unnamed: 0', axis=1)
+        frame2['RunId'] = frame2['RunId'].map(lambda x: x + max_id)
+        max_id = max(max_id, frame2['RunId'].max())
+        frame = pd.concat([ frame, frame2 ])
+    return frame
 
 if __name__ == '__main__':
     if sys.argv[-1] == '--debug':
         run(debug=True)
-    if sys.argv[-1] == '--split':
-        run(split=10)
+    elif sys.argv[-1] == '--split':
+        run(split=20)
     else:
         run()
